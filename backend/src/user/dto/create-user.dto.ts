5c9a018a11ed95cb9159e2cc276ffb96d27b230a
felipeab-b <felipeabferreira22@gmail.com>
2025-10-25 00:16:56 -0300

    feat: cadastro de users com hash de senha e delete de users

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,20 +1,23 @@
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
-import { User } from "../entities/user.entity";
 
 export class CreateUserDto  {
 
-  @IsEmail()
-  email: string;
+    @IsEmail()
+    email: string;
 
-  @IsString()
-  @MinLength(4)
-  @MaxLength(20)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'A senha deve conter ao menos uma letra maiúscula, uma letra minúscula e um número ou caractere especial.',
-  })
-  password: string;
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'A senha deve conter ao menos uma letra maiúscula, uma letra minúscula e um número ou caractere especial.',
+    })
+    password_hash: string;
 
-  @IsString()
-  name: string;
+    @IsString()
+    username: string;
+
+    @IsString()
+    name: string;
+
 }
 
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password_hash: await bcrypt.hash(createUserDto.password_hash, 10),
+    } 
+
+    const new_user = await this.prisma.users.create({
+      data,
+    });
+
+    return {
+      ...new_user,
+      password_hash: undefined,
+    };
   }
 
   findAll() {
@@ -23,7 +36,9 @@ export class UserService {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return await this.prisma.users.delete({
+      where: { id },
+    });
   }
 }
