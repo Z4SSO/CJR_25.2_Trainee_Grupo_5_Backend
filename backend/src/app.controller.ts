cdfbcb5c300f12ee7dd26d356d123c8fb61700a7
felipeab-b <felipeabferreira22@gmail.com>
2025-10-25 18:30:23 -0300

    feat: decorador para indicar o usuário atual logado

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { IsPublic } from './auth/decorators/is-public.decorator';
+import { CurrentUser } from './auth/decorators/curretn-user.decorator';
+import { User } from './user/entity/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('me')
+  whoAmI(@CurrentUser() user: User) {
+    return "Autenticação bem sucedida! Você está logado como: " + user.name;
+  }
+
 }
