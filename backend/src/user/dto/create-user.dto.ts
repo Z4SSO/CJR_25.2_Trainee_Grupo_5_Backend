799ef53f24361cc70b8fa4e741dc4b6adca05e07
felipeab-b <felipeabferreira22@gmail.com>
2025-10-24 23:28:55 -0300

    feat: validação de entrada de users

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto  {
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'A senha deve conter ao menos uma letra maiúscula, uma letra minúscula e um número ou caractere especial.',
+  })
+  password: string;
+
+  @IsString()
+  name: string;
+}
+
