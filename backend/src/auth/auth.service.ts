440fcc5411b7a7ae83a5709ecfc238d94ff86246
felipeab-b <felipeabferreira22@gmail.com>
2025-10-25 15:34:56 -0300

    feat: geração do token JWT

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { UserPayload } from './models/UserPayload';
+import { User } from 'src/user/entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService,
+                private readonly jwtService: JwtService,
+    ) {}
+
+    login(user: User):UserToken {
+        const payload: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            username: user.username,
+            name: user.name,
+        };
+
+        const jwttoken = this.jwtService.sign(payload);
+
+        return {
+            token: jwttoken
+        };
+    }
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findbyEmail(email);
