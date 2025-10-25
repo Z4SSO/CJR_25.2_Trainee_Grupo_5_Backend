1dba7bc1ec4c2484605af492c5381daf4ee4b3bf
felipeab-b <felipeabferreira22@gmail.com>
2025-10-25 18:14:56 -0300

    feat: implementação da validação do token JWT

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,14 +4,18 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UserModule, 
-            JwtModule.register({
-              secret: process.env.JWT_SECRET,
-              signOptions: { expiresIn: '30d' },
-            }),],
+  imports: [
+    UserModule, 
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
