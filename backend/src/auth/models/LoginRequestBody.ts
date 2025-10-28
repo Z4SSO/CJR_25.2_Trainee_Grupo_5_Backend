8facf3946b5fafa4e1a6a41a9712881e6f40f9f2
felipeab-b <felipeabferreira22@gmail.com>
2025-10-28 16:19:33 -0300

    feat(ux): mensagem de email inválido

--- backend/src/auth/models/LoginRequestBody.ts
+++ backend/src/auth/models/LoginRequestBody.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginRequestBody {
-  @IsEmail()
+  @IsEmail({}, { message: 'Insira um endereço de e‑mail válido.' })
   email: string;
 
   @IsString()
