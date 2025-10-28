a909f29635183bbe71d1001a10847810d125f574
markvncs <vmarcu090@gmail.com>
2025-10-28 18:27:55 -0300

    feat: crud de imagens implementada

--- backend/src/products-images/products-images.controller.ts
+++ backend/src/products-images/products-images.controller.ts
@@ -1,25 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ProductsImagesService } from './products-images.service';
 import { CreateProductsImageDto } from './dto/create-products-image.dto';
 import { UpdateProductsImageDto } from './dto/update-products-image.dto';
+import { CurrentUser } from 'src/auth/decorators/curretn-user.decorator';
+import { User } from 'src/user/entity/user.entity';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('products-images')
 export class ProductsImagesController {
   constructor(private readonly productsImagesService: ProductsImagesService) {}
 
-  @Post()
-  create(@Body() createProductsImageDto: CreateProductsImageDto) {
-    return this.productsImagesService.create(createProductsImageDto);
+  @Post('product/:productId')
+  create(
+    @Param('productId', ParseIntPipe) productId: number,
+    @CurrentUser() user: User,
+    @Body() data: CreateProductsImageDto) {
+    return this.productsImagesService.create(data, productId, user.id);
   }
 
-  @Get()
-  findAll() {
-    return this.productsImagesService.findAll();
+  @IsPublic()
+  @Get('product/:productId')
+  findAll(
+    @Param('productId', ParseIntPipe) productId: number) {
+    return this.productsImagesService.findAll(productId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsImagesService.findOne(+id);
+  findOne(
+    @Param('id', ParseIntPipe) id: number) {
+    return this.productsImagesService.findOne(id);
   }
 
   @Patch(':id')
--- backend/src/products-images/products-images.service.ts
+++ backend/src/products-images/products-images.service.ts
@@ -1,26 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductsImageDto } from './dto/create-products-image.dto';
 import { UpdateProductsImageDto } from './dto/update-products-image.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ProductsImagesService {
-  create(createProductsImageDto: CreateProductsImageDto) {
-    return 'This action adds a new productsImage';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateProductsImageDto, productId: number, userId: number ) {
+    const product = await this.prisma.products.findUnique({
+      where: { id: productId },
+      include: {
+        store: true,
+      }
+    });
+    if (!product) {
+      throw new NotFoundException('Produto não encontrado')
+    }
+
+    if (product.store.user_id !== userId) {
+      throw new ForbiddenException(
+        'Você não tem permissão para adicionar imagens a este produto'
+      );
+    }
+
+    return this.prisma.productImages.create({
+      data: {
+        ...data,
+        product_id: productId,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all productsImages`;
+  async findAll(productId: number) {
+    const product = await this.prisma.products.findUnique({
+      where: { id: productId }
+    });
+    if (!product) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+
+    return this.prisma.productImages.findMany({
+      where: { product_id: productId },
+      orderBy: { order: 'asc' },
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productsImage`;
+  async findOne(ProductImageId: number) {
+    const ImagemProduto = await this.prisma.productImages.findUnique({
+      where: {id: ProductImageId}
+    })
+    if (!ImagemProduto) {
+      throw new NotFoundException("Imagem não encontrada");
+    }
+    return ImagemProduto;
   }
 
-  update(id: number, updateProductsImageDto: UpdateProductsImageDto) {
-    return `This action updates a #${id} productsImage`;
+  async update(ProductImageId: number, data: UpdateProductsImageDto, userId: number) {
+    const ImagemProduto = await this.prisma.productImages.findUnique({
+      where: { id: ProductImageId },
+      include: {
+        product: { include: { store: true } }
+      }
+    })
+    if (!ImagemProduto) {
+      throw new NotFoundException("Imagem não encontrada")
+    }
+
+    if (ImagemProduto.product.store.user_id !== userId) {
+      throw new ForbiddenException("Você não tem permissão para editar esta imagem")
+    }
+
+    return this.prisma.productImages.update({
+      where: { id: ProductImageId },
+      data,
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} productsImage`;
+  async remove(ProductImageId: number, userId: number) {
+    const ImagemProduto = await this.prisma.productImages.findUnique({
+      where: { id: ProductImageId },
+      include: {
+        product: { include: { store: true } }
+      }
+    })
+    if (!ImagemProduto) {
+      throw new NotFoundException('Imagem não encontrada')
+    }
+    if (ImagemProduto.product.store.user_id !== userId){
+      throw new ForbiddenException("Você não tem permissão para deletar esta imagem")
+    }
+    return this.prisma.productImages.delete({
+      where: { id: ProductImageId }
+    })
   }
 }
