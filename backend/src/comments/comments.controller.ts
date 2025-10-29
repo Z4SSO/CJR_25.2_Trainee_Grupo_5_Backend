94be9972a1bac969516d7cde95903dcbb113e757
markvncs <vmarcu090@gmail.com>
2025-10-29 00:07:15 -0300

    feat: verificação da autenticação na crud de comentários e atualização das rotas

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,46 +1,56 @@
-import { Controller, Get, Post, Body, Param, Delete, Put, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, ParseIntPipe, Patch } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import type { CommentDto } from './comment.dto';
 import { CurrentUser } from 'src/auth/decorators/curretn-user.decorator';
 import { User } from 'src/user/entity/user.entity';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
 
-  @Post("store-rating/:ratingid")
-  async createStoreRatingComment(@Param("ratingid", ParseIntPipe) ratingid: number,
+  @Post("store-rating/:ratingId")
+  async createStoreRatingComment(
+    @Param("ratingId", ParseIntPipe) ratingId: number,
     @Body() data: CommentDto,
     @CurrentUser() user: User) {
-    return this.commentsService.createStoreRatingComment(data, ratingid, user.id);
+    return this.commentsService.createStoreRatingComment(data, ratingId, user.id);
   }
 
-  @Post("product-rating/:ratingid")
-  async createProductRatingComment(@Param("ratingid", ParseIntPipe) ratingid: number,
+  @Post("product-rating/:ratingId")
+  async createProductRatingComment(
+    @Param("ratingId", ParseIntPipe) ratingId: number,
     @Body() data: CommentDto,
     @CurrentUser() user: User) {
-    return this.commentsService.createProductRatingComment(data, ratingid, user.id);
+    return this.commentsService.createProductRatingComment(data, ratingId, user.id);
   }
 
-
-  @Get()
-  async findAll() {
-    return this.commentsService.findAll();
+  @IsPublic()
+  @Get('store-rating/:ratingId')
+  async findAllStoreRating(@Param('ratingId', ParseIntPipe) ratingId: number) {
+    return this.commentsService.findAllStoreRating(ratingId);
   }
 
-  @Put(':id')
-  async update(@Param('id') id: number, @Body() data: CommentDto) {
-    return this.commentsService.update(Number(id), data);
+  @IsPublic()
+  @Get('product-rating/:ratingId')
+  async findAllProductRating(@Param('ratingId', ParseIntPipe) ratingId: number) {
+    return this.commentsService.findAllProductRating(ratingId);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number, 
+    @CurrentUser() user: User,
+    @Body() data: CommentDto) {
+    return this.commentsService.update(id, data, user.id);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: number) {
-    return this.commentsService.delete(Number(id));
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: number) {
-    return this.commentsService.findOne(Number(id));
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @CurrentUser() user: User,
+  ) {
+    return this.commentsService.delete(id, user.id);
   }
 
 
--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CommentDto } from './comment.dto';
 import { PrismaService } from 'src/database/prisma.service';
 
@@ -8,15 +8,15 @@ export class CommentsService {
   constructor(private prisma: PrismaService) { }
 
   async createStoreRatingComment(data: CommentDto, StoreRatingId: number, UserId: number) {
-    const StoreRating = await this.prisma.StoreRatings.findUnique({
+    const StoreRating = await this.prisma.storeRatings.findUnique({
       where: { id: StoreRatingId }
     })
 
     if (!StoreRating) {
-      throw new NotFoundException("Avaliacao nao encontrada")
+      throw new NotFoundException("Avaliação não encontrada")
     }
 
-    return this.prisma.RatingComments.create({
+    return this.prisma.ratingComments.create({
       data: {
         ...data,
         user_id: UserId,
@@ -26,15 +26,15 @@ export class CommentsService {
   }
 
   async createProductRatingComment(data: CommentDto, ProductRatingId: number, UserId: number) {
-    const ProductRating = await this.prisma.ProductRatings.findUnique({
+    const ProductRating = await this.prisma.productRatings.findUnique({
       where: { id: ProductRatingId }
     })
 
     if (!ProductRating) {
-      throw new NotFoundException("Avaliacao nao encontrada")
+      throw new NotFoundException("Avaliacao não encontrada")
     }
 
-    return this.prisma.RatingComments.create({
+    return this.prisma.ratingComments.create({
       data: {
         ...data,
         user_id: UserId,
@@ -43,44 +43,62 @@ export class CommentsService {
     })
   }
 
-
-
-  async findAll() {
-    return this.prisma.ratingComment.findMany();
+  async findAllStoreRating(ratingId: number) {
+    const rating = await this.prisma.storeRatings.findUnique({
+      where: { id: ratingId }
+    })
+    if (!rating) {
+      return new NotFoundException("Avaliação não encontrada")
+    }
+    return this.prisma.ratingComments.findMany({
+      where: { store_rating_id: ratingId },
+      include: { user: { select : { username: true, profile_picture_url: true } } },
+    });
   }
 
-  async update(id: number, data: CommentDto) {
-    const commentexists = await this.prisma.ratingComment.findUnique({
+  async findAllProductRating(ratingId: number) {
+    const rating = await this.prisma.productRatings.findUnique({
+      where: { id: ratingId }
+    })
+    if (!rating) {
+      return new NotFoundException("Avaliação não encontrada")
+    }
+    return this.prisma.ratingComments.findMany({
+      where: { store_rating_id: ratingId },
+      include: { user: { select : { username: true, profile_picture_url: true } } },
+    });
+  }
+
+  async update(id: number, data: CommentDto, userId: number) {
+    const commentexists = await this.prisma.ratingComments.findUnique({
       where: { id }
     })
     if (!commentexists) {
-      throw new Error("comment not found")
+      throw new NotFoundException("Comentário não encontrado")
     }
-    return await this.prisma.ratingComment.update({
-      data,
+
+    if (commentexists.user_id !== userId) {
+      throw new ForbiddenException("Você não tem permissão para editar esse comentário")
+    }
+    return await this.prisma.ratingComments.update({
+      data: data,
       where: { id }
     })
   }
 
-  async delete(id: number) {
-    const commentexists = await this.prisma.ratingComment.findUnique({
+  async delete(id: number, userId: number) {
+    const commentexists = await this.prisma.ratingComments.findUnique({
       where: { id }
     })
     if (!commentexists) {
-      throw new Error("comment not found")
+      throw new NotFoundException("Comentário não encontrado")
     }
-    return await this.prisma.ratingComment.delete({
-      where: { id }
-    })
-  }
-
-  async findOne(id: number) {
-    const commentexists = await this.prisma.ratingComment.findUnique({
-      where: { id }
-    })
-    if (!commentexists) {
-      throw new Error("comment not found")
+    
+    if (commentexists.user_id !== userId) {
+      throw new ForbiddenException("Você não tem permissao para deletar esse comentário")
     }
-    return commentexists;
+    return await this.prisma.ratingComments.delete({
+      where: { id },
+    });
   }
 }
