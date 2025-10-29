e6e8de52921fbfacd29306821890b1774fb5349b
Caio Cordeiro de Sousa <caiocord2005@gmail.com>
2025-10-28 23:05:41 -0300

    feat: integrar crud de comentarios

--- backend/src/comments/comment.dto.ts
+++ backend/src/comments/comment.dto.ts
@@ -1,18 +1,6 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CommentDto {
-
-  @IsNumber()
-  @IsNotEmpty()
-  user_id: number;
-
-  @IsOptional()
-  @IsNumber()
-  store_rating_id?: number;
-
-  @IsOptional()
-  @IsNumber()
-  product_rating_id?: number;
 
   @IsString()
   @IsNotEmpty()
--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, ParseIntPipe } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import type { CommentDto } from './comment.dto';
+import { CurrentUser } from 'src/auth/decorators/curretn-user.decorator';
+import { User } from 'src/user/entity/user.entity';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
 
-  @Post()
-  async create(@Body() data: CommentDto) {
-    return this.commentsService.create(data);
+  @Post("store-rating/:ratingid")
+  async createStoreRatingComment(@Param("ratingid", ParseIntPipe) ratingid: number,
+    @Body() data: CommentDto,
+    @CurrentUser() user: User) {
+    return this.commentsService.createStoreRatingComment(data, ratingid, user.id);
   }
+
+  @Post("product-rating/:ratingid")
+  async createProductRatingComment(@Param("ratingid", ParseIntPipe) ratingid: number,
+    @Body() data: CommentDto,
+    @CurrentUser() user: User) {
+    return this.commentsService.createProductRatingComment(data, ratingid, user.id);
+  }
+
 
   @Get()
   async findAll() {
