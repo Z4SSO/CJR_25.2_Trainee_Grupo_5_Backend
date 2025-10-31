3c3958c54d8b26accc48308266f3b95b1df5b4dc
felipeab-b <felipeabferreira22@gmail.com>
2025-10-31 16:37:33 -0300

    fix: error cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,11 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
